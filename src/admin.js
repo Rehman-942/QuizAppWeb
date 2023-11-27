0692bec2df72658e209df1d7155c7d013a73f34d
Rehman-942 <rehmanafzal942@gmail.com>
2023-11-27 22:48:56 +0500

    adding debuger

--- src/admin.js
+++ src/admin.js
@@ -22,7 +22,7 @@ const Admin = () => {
         // response.json()
       // })
       .then((response) => {
-        console.log('response');
+        console.log('response', response);
         setData(response.data)
       }
       ) // Assuming the API returns results in the "results" field
