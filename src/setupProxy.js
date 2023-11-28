333acd9710e1dcd55cc52028ed7ef5d8f334021a
Rehman-942 <rehmanafzal942@gmail.com>
2023-11-28 23:11:43 +0500

    proxy updated

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,5 +1,5 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
-const hostUrl = process.env.API_HOST || "http://localhost:3001"
+const hostUrl = process.env.API_HOST || "https://localhost:3001"
 
 module.exports = function (app) {
   app.use(
