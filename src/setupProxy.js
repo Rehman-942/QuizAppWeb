4da535e0d1b941e56fc405bb42d691ae7ec01474
Rehman-942 <rehmanafzal942@gmail.com>
2023-11-29 00:04:39 +0500

    remove unused

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,11 +1,11 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
-const hostUrl = process.env.API_HOST || "http://localhost:3001"
+
 
 module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: hostUrl,
+      target: "http://localhost:3001",
       changeOrigin: true,
       pathRewrite: {
         '^/api/': '/'
