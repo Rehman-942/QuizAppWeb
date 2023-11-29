616052cf63f6976f56d7bf750be64fef99054b34
Rehman-942 <rehmanafzal942@gmail.com>
2023-11-29 22:48:56 +0500

    updated proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,14 +1,24 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = function(app) {
+module.exports = function (app) {
+  // Proxy requests for the API to the backend server
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://test.yucatanblue.com:3001',
+      target: 'http://test.yucatanblue.com:3001',  // Adjust the target URL
       changeOrigin: true,
       pathRewrite: {
         '^/api/': '/'
-      }
+      },
+      secure: false,  // Disable SSL certificate validation (use only during development)
+    })
+  );
+
+  app.use(
+    createProxyMiddleware({
+      target: 'http://test.yucatanblue.com:3001',  // Adjust the target URL
+      changeOrigin: true,
+      secure: false,  // Disable SSL certificate validation (use only during development)
     })
   );
 };
